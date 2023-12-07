fcf0112b7703ce975cf28b28aba455d2737c6475
Sourav Biswas <sourav.sb56@gmail.com>
2023-12-07 14:38:07 +0530

    Basic Setup

---
+++ frontend/src/components/Navbar.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import { Link as ReactRouterLink } from "react-router-dom";
+import { Link as ChakraLink, Container } from "@chakra-ui/react";
+
+const Navbar = () => {
+  return (
+    <Container>
+      <ChakraLink as={ReactRouterLink} to="/">
+        Home
+      </ChakraLink>
+      <ChakraLink as={ReactRouterLink} to="/login">
+        Login
+      </ChakraLink>
+    </Container>
+  );
+};
+
+export default Navbar;
---
+++ frontend/src/pages/Login.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { Input, Container, Heading } from "@chakra-ui/react";
+
+const Home = () => {
+  return (
+    <Container>
+      <Heading>
+        Login
+      </Heading>
+    </Container>
+  );
+};
+
+export default Home;
